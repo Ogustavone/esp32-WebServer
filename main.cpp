5254cc4951cc792e77496339075df709da2acd5d
Gustavo S. <ogustavone@gmail.com>
2025-01-16 00:52:34 -0300

    update setup function
    wifi connection handler

--- main.cpp
+++ main.cpp
@@ -2,8 +2,24 @@
 // #include <WiFi.h>
 // #include <WebServer.h>
 
+const char* ssid = "nome da rede";
+const char* password = "senha da rede";
+
 void setup() {
-  // Função principal
+  pinMode(LED_BUILTIN, OUTPUT);  // define o pino do led como saída
+  digitalWrite(LED_BUILTIN, LOW);
+
+  Serial.begin(115200);  // Serial com baudrate 115200 (esp32)
+  WiFi.mode(WIFI_STA);
+
+  // Conexão wifi
+  WiFi.begin( ssid, password );
+  Serial.println("Conectando a rede WiFi..");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Conectado: " + String(ssid));
 }
 
 void loop() {
