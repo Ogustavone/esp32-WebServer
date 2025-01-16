119d59d510421621be9b0b4db13962a74b8b375d
Gustavo S. <ogustavone@gmail.com>
2025-01-16 00:57:18 -0300

    added webserver handler

--- main.cpp
+++ main.cpp
@@ -14,14 +14,26 @@ void setup() {
 
   // Conexão wifi
   WiFi.begin( ssid, password );
+  
   Serial.println("Conectando a rede WiFi..");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
+
   Serial.println("Conectado: " + String(ssid));
+
+  // Webserver
+  WebServer server(80);
+
+  server.on("/", rootHandler);
+  server.on("/led-on", HTTP_GET, [](){ ledHandler(true); });
+  server.on("/led-off", HTTP_GET, [](){ ledHandler(false); });
+
+  server.begin();
+  Serial.println("Servidor iniciado, acesse em http://" + String(WiFi.localIP()));
 }
 
 void loop() {
-  // Função em loop
+  server.handleClient();
 }
